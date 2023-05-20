6ac61b732fc6a05fa7f4532a8594a0aea39fce78
Jagadesh-Ronanki <jagadesh.ronanki@gmail.com>
2023-05-20 21:11:11 +0530

    socialsComments::2023-05-20

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,12 @@ const Footer = () => {
       <p className="text-md text-[#b1c0ef] text-[20px] font-semibold pb-4">Dark Shadowy Super Coders</p>
       <ul className="text-white text-[8px] md:text-[18px] flex flex-row justify-evenly w-full max-w-5xl">
         <li className="after:content-['|'] after:text-gray-600 after:mx-2">An open organization</li>
+        { /*
+         * Change href's to your organization's handle from `line 14-20``
+         * from: href="https://twitter.com/"
+         * to: href="https://twitter.com/@DSSC"
+          */}
+        <li className="after:content-['|'] after:text-gray-600 after:mx-2"><a href="https://twitter.com/" target="_blank" rel="noreferrer" aria-label="Mail (Opens in new Window)" className="text-purple-400 hover:text-purple-600 focus:text-purple-600 focus:outline-none">Mail</a></li>
         <li className="after:content-['|'] after:text-gray-600 after:mx-2"><a href="https://twitter.com/" target="_blank" rel="noreferrer" aria-label="Twitter (Opens in new Window)" className="text-purple-400 hover:text-purple-600 focus:text-purple-600 focus:outline-none">Twitter</a></li>
         <li className="after:content-['|'] after:text-gray-600 after:mx-2"><a href="https://discord.gg/" target="_blank" rel="noreferrer" aria-label="Discord (Opens in new Window)" className="text-purple-400 hover:text-purple-600 focus:text-purple-600 focus:outline-none">Discord</a></li>
         <li className="after:content-['|'] after:text-gray-600 after:mx-2"><a href="https://github.com//" target="_blank" rel="noreferrer" aria-label="GitHub (Opens in new Window)" className="text-purple-400 hover:text-purple-600 focus:text-purple-600 focus:outline-none">GitHub</a></li>
